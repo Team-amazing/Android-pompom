b8db1822e431d608e5447e9205bc53928b9f7cb8
parkUiery <parkuiery@gmail.com>
2023-05-18 23:41:21 +0900

    ✨ :: 액티비티 추가

--- outopompomme/app/src/main/java/com/example/outopompomme/FunctionFragment.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/FunctionFragment.java
@@ -1,9 +1,11 @@
 package com.example.outopompomme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,9 +13,24 @@ import androidx.fragment.app.Fragment;
 public class FunctionFragment extends Fragment {
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main_menu_function, container, false);
 
-        return inflater.inflate(R.layout.fragment_main_menu_function, container, false);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), GivewaterActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        Button button = rootView.findViewById(R.id.give_water_btn);
+        button.setOnClickListener(clickListener);
+
+        return rootView;
     }
+
+
 }
