729c53ba8ab00aa55c1e705f240e2bc5399fecfc
parkUiery <parkuiery@gmail.com>
2023-05-19 19:15:14 +0900

    ✨ :: 버튼 누르면 새로운 액티비티로 넘어가기

---
+++ outopompomme/app/src/main/java/com/example/outopompomme/OpendoorActivity.java
@@ -0,0 +1,14 @@
+package com.example.outopompomme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpendoorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opendoor);
+    }
+}
