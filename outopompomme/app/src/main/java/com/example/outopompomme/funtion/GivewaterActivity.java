3bfcfeff97e07c7932e3ebd58bc2068eb4f21de0
parkUiery <parkuiery@gmail.com>
2023-05-28 21:35:20 +0900

    refactor :: 코드 수정

--- outopompomme/app/src/main/java/com/example/outopompomme/funtion/GivewaterActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/funtion/GivewaterActivity.java
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.outopompomme.R;
 
+
 public class GivewaterActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_givewater);
+
     }
 }
