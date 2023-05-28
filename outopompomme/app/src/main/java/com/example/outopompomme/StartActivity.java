b0027dc988f130ac608ec313f57f4aa836ec86ef
parkUiery <parkuiery@gmail.com>
2023-05-29 01:57:27 +0900

    perf :: 팜팜이 첫 화면 추가

---
+++ outopompomme/app/src/main/java/com/example/outopompomme/StartActivity.java
@@ -0,0 +1,40 @@
+package com.example.outopompomme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button start_start_btn = findViewById(R.id.start_start_btn);
+
+        TextView start_login = findViewById(R.id.start_login);
+
+        start_start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        start_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
