ce89759b3c5ae0d02996505f2203b066f3dd90a7
parkUiery <parkuiery@gmail.com>
2023-05-29 21:44:47 +0900

    perf :: 마이페이지 기능 추가

--- outopompomme/app/src/main/java/com/example/outopompomme/home/MypageActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/home/MypageActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.outopompomme.DiaryActivity;
 import com.example.outopompomme.MainActivity;
+import com.example.outopompomme.MyinfoActivity;
+import com.example.outopompomme.PlantTypesActivity;
 import com.example.outopompomme.R;
 import com.example.outopompomme.StartActivity;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +25,14 @@ public class MypageActivity extends AppCompatActivity {
 
         Button mypage_daiary_btn = findViewById(R.id.mypage_daiary_btn);
 
+        Button mypage_plantBtn = findViewById(R.id.mypage_plantBtn);
+
         findViewById(R.id.mypage_logoutBtn).setOnClickListener(onClickListener);
+
+        ImageButton mypage_settingBtn = findViewById(R.id.mypage_settingBtn);
+
+        ImageButton mypage_backkey = findViewById(R.id.mypage_backkey);
+
 
 
         mypage_daiary_btn.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +41,30 @@ public class MypageActivity extends AppCompatActivity {
                 Intent intent = new Intent(MypageActivity.this , DiaryActivity.class);
                 startActivity(intent);
 
+            }
+        });
+
+        mypage_plantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, PlantTypesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mypage_settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, MyinfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mypage_backkey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -46,6 +80,8 @@ public class MypageActivity extends AppCompatActivity {
             }
         }
     };
+
+
 
     private void startActivity() {
         Intent intent = new Intent(MypageActivity.this, StartActivity.class);
