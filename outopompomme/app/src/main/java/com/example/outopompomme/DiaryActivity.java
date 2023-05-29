f8ad450ec466ed8a939fc03fcc2c8fcf094fa2ca
parkUiery <parkuiery@gmail.com>
2023-05-29 21:54:37 +0900

    perf :: 다이어리 뒤로가기 추가

--- outopompomme/app/src/main/java/com/example/outopompomme/DiaryActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/DiaryActivity.java
@@ -2,7 +2,12 @@ package com.example.outopompomme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.outopompomme.home.MypageActivity;
 
 public class DiaryActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class DiaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        ImageButton diary_backkey = findViewById(R.id.diary_backkey);
+
+        diary_backkey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiaryActivity.this, MypageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
