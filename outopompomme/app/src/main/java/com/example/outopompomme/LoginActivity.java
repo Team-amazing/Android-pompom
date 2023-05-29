998d7dca31cb0563458a1f743b2476ce428f518d
parkUiery <parkuiery@gmail.com>
2023-05-29 23:30:58 +0900

    refactor :: 로그인 코드 수정

--- outopompomme/app/src/main/java/com/example/outopompomme/LoginActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.example.outopompomme;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,65 +20,64 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "SignUpActivity";
     private FirebaseAuth mAuth;
-
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.login_loginBtn:
+                    logIn();
+                    break;
+                case R.id.login_passwordresetBtn:
+                    myStartActivity(MyinfoActivity.class);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.login_loginBtn).setOnClickListener(onClickListener);
+        findViewById(R.id.login_passwordresetBtn).setOnClickListener(onClickListener);
     }
 
-
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.login_loginBtn:
-                    logIn();
-                    break;
-            }
-        }
-    };
-
-
     private void logIn() {
-        String email = ((EditText)findViewById(R.id.login_email)).getText().toString();
-        String password = ((EditText)findViewById(R.id.login_password)).getText().toString();
+        String email = ((EditText) findViewById(R.id.login_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.login_password)).getText().toString();
 
-        if(email.length() >0&& password.length() >0){
+        if (email.length() > 0 && password.length() > 0) {
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                strtToast("로그인에 성공했습니다.");
-                                gotoMainActivity();
+                                startToast("로그인에 성공했습니다.");
+                                finish();
                             } else {
-                               if(task.getException() != null){
-                                   strtToast(task.getException().toString());
-                               }
+                                if (task.getException() != null) {
+                                    startToast(task.getException().toString());
+                                }
 
                             }
                         }
                     });
 
-        }else{
-            strtToast("이메일 또는 비밀번호를 입력하세요");
+        } else {
+            startToast("이메일 또는 비밀번호를 입력하세요");
         }
     }
 
-    private void strtToast(String msg) {
-        Toast.makeText(this, msg , Toast.LENGTH_SHORT).show();
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void gotoMainActivity() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(LoginActivity.this, c);
         intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
