8e1f6bfaabc9c20abafebbc092f75cc5d3d9dfeb
parkUiery <parkuiery@gmail.com>
2023-05-30 20:47:31 +0900

    perf :: 아두이노 연결 버튼 추가

--- outopompomme/app/src/main/java/com/example/outopompomme/home/MypageActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/home/MypageActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.outopompomme.DiaryActivity;
+import com.example.outopompomme.LinkarduinoActivity;
 import com.example.outopompomme.MainActivity;
 import com.example.outopompomme.MyinfoActivity;
 import com.example.outopompomme.PlantTypesActivity;
@@ -26,6 +27,8 @@ public class MypageActivity extends AppCompatActivity {
         Button mypage_daiary_btn = findViewById(R.id.mypage_daiary_btn);
 
         Button mypage_plantBtn = findViewById(R.id.mypage_plantBtn);
+
+        Button mypage_link_arduino = findViewById(R.id.mypage_link_arduino);
 
         findViewById(R.id.mypage_logoutBtn).setOnClickListener(onClickListener);
 
@@ -48,6 +51,14 @@ public class MypageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MypageActivity.this, PlantTypesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mypage_link_arduino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, LinkarduinoActivity.class);
                 startActivity(intent);
             }
         });
