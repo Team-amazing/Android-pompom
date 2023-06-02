8ae06407a5aedaae2158f1a49daf15007108f7ed
parkUiery <parkuiery@gmail.com>
2023-06-02 20:28:23 +0900

    refactor :: 회원 정보 수정

--- outopompomme/app/src/main/java/com/example/outopompomme/MyinfoActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/MyinfoActivity.java
@@ -1,23 +1,27 @@
 package com.example.outopompomme;
 
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.outopompomme.home.MypageActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 
 public class MyinfoActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
 
 
     @Override
@@ -25,58 +29,59 @@ public class MyinfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myinfo);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        findViewById(R.id.myinfo_send).setOnClickListener(onClickListener);
-
-        ImageButton myinfo_backkey = findViewById(R.id.myinfo_backkey);
 
 
-        myinfo_backkey.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MyinfoActivity.this, MypageActivity.class);
-                startActivity(intent);
+                switch (v.getId()) {
+                    case R.id.myinfo_checkBtn:
+                        profileUpdate();
+                        break;
+
+                }
             }
-        });
+        };
+
+        findViewById(R.id.myinfo_checkBtn).setOnClickListener(onClickListener);
+
+
+
     }
 
+    private void profileUpdate() {
+        String id = ((EditText) findViewById(R.id.myinfo_idEt)).getText().toString();
+        String password = ((EditText) findViewById(R.id.login_password)).getText().toString();
 
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.myinfo_send:
-                    send();
-                    break;
-            }
-        }
-    };
+        if (id.length() > 0 ) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(id)
+                    .build();
+            if(user != null){
+                user.updateProfile(profileUpdates)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    startToast("회원 정보 등록에 성공하셨습니다.");
 
-
-    private void send() {
-        String email = ((EditText)findViewById(R.id.login_email)).getText().toString();
-
-        if(email.length() >0){
-            mAuth.sendPasswordResetEmail(email)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                strtToast("이메일을 보냈습니다.");
+                                }
                             }
-                        }
-                    });
+                        });
+            }
 
-        }else{
-            strtToast("이메일을 입력하세요");
+        } else {
+            startToast("회원 정보를 입력해주세요");
         }
     }
 
-    private void strtToast(String msg) {
-        Toast.makeText(this, msg , Toast.LENGTH_SHORT).show();
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
+
 
 
 }
