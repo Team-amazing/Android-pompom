ff12e0f6e0b90ee534bf401d94b85ec6311e5c65
parkUiery <parkuiery@gmail.com>
2023-06-02 20:25:11 +0900

    perf :: 날씨 정보 추가

--- outopompomme/app/src/main/java/com/example/outopompomme/funtion/FunctionFragment.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/funtion/FunctionFragment.java
@@ -6,16 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.outopompomme.ApiTest;
 import com.example.outopompomme.R;
+import com.example.outopompomme.RecyclerViewWeatherAdapter;
+import com.example.outopompomme.RecyclerViewWeatherItem;
 
-import org.json.JSONException;
-
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 
 
@@ -23,11 +24,13 @@ public class FunctionFragment extends Fragment {
 
     private ApiTest at;
 
-
-
-    public FunctionFragment(){
-
-    }
+    private RecyclerView mRecyclerView;
+    private ArrayList<RecyclerViewWeatherItem> mList;
+    private RecyclerViewWeatherAdapter mRecyclerViewAdapter;
+    private Button give_water_btn;
+    private Button light_btn;
+    private Button open_door_btn;
+    private Button water_box_btn;
 
     //api연동
     @Override
@@ -39,23 +42,20 @@ public class FunctionFragment extends Fragment {
             public void run() {
                 try {
                     at.func();
-                } catch (IOException | JSONException e) {
+                } catch (Throwable e) {
                     e.printStackTrace();
                 }
             }
         });
-
     }
-
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main_menu_function, container, false);
 
-        Button give_water_btn = rootView.findViewById(R.id.give_water_btn);
-        Button light_btn = rootView.findViewById(R.id.light_btn);
-        Button open_door_btn = rootView.findViewById(R.id.open_door_btn);
-        Button watter_box_btn = rootView.findViewById(R.id.watter_box_btn);
-
+        give_water_btn = rootView.findViewById(R.id.give_water_btn);
+        light_btn = rootView.findViewById(R.id.light_btn);
+        open_door_btn = rootView.findViewById(R.id.open_door_btn);
+        water_box_btn = rootView.findViewById(R.id.watter_box_btn);
 
 
         give_water_btn.setOnClickListener(new View.OnClickListener() {
@@ -82,7 +82,7 @@ public class FunctionFragment extends Fragment {
             }
         });
 
-        watter_box_btn.setOnClickListener(new View.OnClickListener() {
+        water_box_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), WatertanckActivity.class);
@@ -91,6 +91,35 @@ public class FunctionFragment extends Fragment {
         });
         return rootView;
 
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceSate) {
+        super.onViewCreated(view, savedInstanceSate);
+
+        mRecyclerView = view.findViewById(R.id.weather_recyclerView);
+        mList = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            addItem("DAEJEON", "iconName", "16°C", "WEDNESDAY", "15:37 PM");
+        }
+
+        mRecyclerViewAdapter = new RecyclerViewWeatherAdapter(mList);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
+        //RecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+    }
+
+    public void addItem(String locationText, String imgName, String tempText, String weekText, String timeText) {
+        RecyclerViewWeatherItem item = new RecyclerViewWeatherItem();
+
+        item.setmLocationText(locationText);
+        item.setmImgName(imgName);
+        item.setmTempText(tempText);
+        item.setmWeekText(weekText);
+        item.setmTimeText(timeText);
+
+        mList.add(item);
     }
 
 }
