15871da204aad613aa67e78ba7de1f4daaab2e3b
parkUiery <parkuiery@gmail.com>
2023-06-28 21:30:28 +0900

    refactor :: 코드 삭제

--- outopompomme/app/src/main/java/com/example/outopompomme/home/MyinfoActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/home/MyinfoActivity.java
@@ -1,18 +1,7 @@
 package com.example.outopompomme.home;
 
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.outopompomme.R;
 
 
