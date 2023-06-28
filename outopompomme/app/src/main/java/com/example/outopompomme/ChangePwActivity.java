8ae8d1a0ca400e9a8949481943e3947ed7dbdfaf
parkUiery <parkuiery@gmail.com>
2023-06-28 21:33:46 +0900

    refacotr :: 종속성 변경

--- outopompomme/app/src/main/java/com/example/outopompomme/ChangePwActivity.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/ChangePwActivity.java
@@ -21,7 +21,7 @@ public class ChangePwActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_myinfo);
+        setContentView(R.layout.activity_change_pw);
 
 
 
