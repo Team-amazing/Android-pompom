d919dd92f474d1ca71979f3a50fb41d2012b8be0
parkUiery <parkuiery@gmail.com>
2023-07-02 23:17:15 +0900

    refactor :: 코드 수정

--- outopompomme/app/src/main/java/com/example/outopompomme/first/First1Fragment.java
+++ outopompomme/app/src/main/java/com/example/outopompomme/first/First1Fragment.java
@@ -1,35 +1,24 @@
 package com.example.outopompomme.first;
 
-import static android.content.ContentValues.TAG;
-
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.example.outopompomme.MemberInfo;
 import com.example.outopompomme.R;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Arrays;
 
 
 public class First1Fragment extends Fragment {
@@ -38,13 +27,12 @@ public class First1Fragment extends Fragment {
     private ImageButton nextBtn;
 
 
-
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first1, container, false);
 
-        Log.d("TEST","프래그먼트 인");
+        Log.d("TEST", "프래그먼트 인");
 
         usernikname = view.findViewById(R.id.fragment1_nikname_et);
         nextBtn = view.findViewById(R.id.frament1_next_btn);
@@ -54,24 +42,26 @@ public class First1Fragment extends Fragment {
             public void onClick(View v) {
                 inputnickname();
                 ((UserInfoActivity) requireActivity()).showNextFragment(2);
+                Log.d("TEST", "버튼 클릭");
             }
         });
 
 
-        Log.d("TEST","프래그먼트 끝");
+        Log.d("TEST", "프래그먼트 끝");
         return view;
     }
 
-    private void inputnickname(){
-        String nickname = String.valueOf((EditText) getView().findViewById(R.id.fragment1_nikname_et));
+    private void inputnickname() {
+        EditText nicknameEt = getView().findViewById(R.id.fragment1_nikname_et);
+        String nickname = nicknameEt.getText().toString();
 
-        if(nickname.length()>0) {
+        if (nickname.length() > 0) {
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             FirebaseFirestore db = FirebaseFirestore.getInstance();
 
             MemberInfo memberInfo = new MemberInfo(nickname);
 
-            if(user != null){
+            if (user != null) {
                 db.collection("users").document(user.getUid()).set(memberInfo)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
@@ -87,26 +77,25 @@ public class First1Fragment extends Fragment {
                         });
             }
 
-            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                    .setDisplayName(nickname)
-                    .build();
+//            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+//                    .setDisplayName(nickname)
+//                    .build();
+//
+//            if(user != null){
+//                user.updateProfile(profileUpdates)
+//                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<Void> task) {
+//                                if (task.isSuccessful()) {
+//                                    Log.d(TAG, "User profile updated.");
+//                                }
+//                            }
+//                        });
+//            }
 
-            if(user != null){
-                user.updateProfile(profileUpdates)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Log.d(TAG, "User profile updated.");
-                                }
-                            }
-                        });
-            }
-
-        }else{
-            Toast.makeText(this.getContext().getApplicationContext(), "닉네임을 입력해주세요",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this.getContext().getApplicationContext(), "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show();
         }
-
 
 
     }
